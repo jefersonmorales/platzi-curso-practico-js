8d80880a17070f7913001867fc9c4da895713858
jfermorales <jfermorales28@outlook.com>
2021-10-29 23:38:37 -0500

    Interactuando con JS desde HTML

--- README.md
+++ README.md
@@ -3,8 +3,3 @@
 ...
 
 ## Talle #1: figuras geometricas
-
--Primer paso: definir las formulas
--Segundo paso: implementar las formulas en javascript
--Tercer paso: crear funciones
--Cuarto paso: integrar JS con HTML
--- figuras.js
+++ figuras.js
@@ -38,3 +38,21 @@ function perimetroCirculo(radio) {
 function areaCirculo(radio){
     return (radio * radio) * PI;
 }
+
+
+// Aqui interactuamos con el html
+function calcularPerimetroCuadrado() {
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+
+function calcularAreaCuadrado() {
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+
+    const area = areaCuadrado(value);
+    alert(area);
+}
